dc2d405589f0753769108b3e45628beca042e50f
Keithyang <123227682@qq.com>
2018-08-09 16:56:25 +0800

    service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import {HEROES} from './heroes/mock-heros';
 import {Hero} from './heroes/hero';
 import {observable,of, Observable} from 'rxjs';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   getHeroes():Observable<Hero[]> {
+    this.messageService.add('HeroService:Fetched heroes');
     return of(HEROES);
   }
 
-  constructor() { }
+  constructor(private messageService:MessageService) { }
 }
